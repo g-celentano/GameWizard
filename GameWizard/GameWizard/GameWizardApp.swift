95855fff6b44662372b0b56b16c75325e2a3f669
g-celentano <gaetano99celentano@gmail.com>
2023-02-16 14:07:35 +0100

    First Commit

---
+++ GameWizard/GameWizard/GameWizardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameWizardApp.swift
+//  GameWizard
+//
+//  Created by Gaetano Celentano on 15/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GameWizardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
