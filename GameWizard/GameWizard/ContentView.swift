681414d3536e1beca93244d2b4fec615af23c547
Letterio Ugo Cangiano <letterio@live.it>
2023-03-02 17:26:15 +0100

    Added onboarding logic, design is tbd.

--- GameWizard/GameWizard/ContentView.swift
+++ GameWizard/GameWizard/ContentView.swift
@@ -14,12 +14,18 @@ let global_width  = UIScreen.main.bounds.width
 let games : [Game] = load("games")
 let recommender : Recommender = Recommender()
 
+var totalViews = 1
+
 struct ContentView: View {
-    
-   
+   @AppStorage("currentView") var currentView = 1
     var body: some View {
+        
+        if currentView == 1 {
+            OnboardingView()
+        } else if currentView == 2 {
             ChatView()
         }
+    }
     
     
         struct ContentView_Previews: PreviewProvider {
